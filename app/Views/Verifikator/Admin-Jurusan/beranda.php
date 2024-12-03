ec3ef1e0d7337f9c6d490d5bbdb0c62c9b3e066f
fateehhh <siyaapppp@gmail.com>
2024-12-03 21:51:28 +0700

    update verifikator-jurusan

---
+++ app/Views/Verifikator/Admin-Jurusan/beranda.php
@@ -0,0 +1,10 @@
+<div class="tab-content" id="beranda">
+    <div class="welcome">
+        <h1>Selamat Datang, Jiha</h1>
+        <p>Anda berada di halaman pengajuan bebas tanggungan</p>
+    </div>
+    <div class="chart-container">
+        <div class="chart-title">Status Proses Verifikasi</div>
+        <canvas id="chartjs-bar"></canvas>
+    </div>
+</div>
---
+++ app/Views/layouts/sidebar_ver.php
@@ -0,0 +1,15 @@
+<link rel="stylesheet" href="../../../public/css/sidebar.css">
+<div class="sidebar">
+        <a class="menu-item active" href="#beranda" onclick="showTab('beranda')">
+            <i class="bi bi-house"></i> Beranda
+        </a>
+        <a class="menu-item" href="#">
+            <i class="bi bi-patch-check"></i> Verifikasi Tanggungan
+        </a>
+        <a class="menu-item" href="#riwayat" onclick="showTab('riwayat')">
+            <i class="bi bi-clock-history"></i> Riwayat Verifikasi
+        </a>
+        <a class="menu-item logout" href="../login.php">
+            <i class="bi bi-power"></i> Keluar
+        </a>
+    </div>
